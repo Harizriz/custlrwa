ae32effa828ff472db0576bb44158de621573ffc
GeraldK <geraldkan99@gmail.com>
2019-11-28 11:28:37 +0800

    changes to fix bugs in models, added null args for fields

--- cwabackend/site_scraper/models.py
+++ cwabackend/site_scraper/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 # Create your models here.
 class StoredResult(models.Model):
-    product_name = models.TextField()
-    sizing = models.CharField(max_length = 5) #for sizes like xxxxl
-    price = models.FloatField()
-    currency = models.CharField(max_length = 3) #currency like USD, MYR, JPY, THB, etc.
-    brand = models.CharField(max_length = 50)
-    image_url = models.URLField(NULL = True, max_length = 300)
+    product_name = models.TextField(null = True)
+    sizing = models.CharField(null = True, max_length = 5) #for sizes like xxxxl
+    price = models.FloatField(null = True)
+    currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
+    brand = models.CharField(null = True, max_length = 50)
+    image_url = models.URLField(null = True, max_length = 300)
 
-class ScrapedResult(model.Model):
-    product_name = models.TextField()
-    sizing = models.CharField(max_length = 5) #for sizes like xxxxl
-    price = models.FloatField()
-    currency = models.CharField(max_length = 3) #currency like USD, MYR, JPY, THB, etc.
-    brand = models.CharField(max_length = 50)
-    image_url = models.URLField(NULL = True, max_length = 300)
+class ScrapedResult(models.Model):
+    product_name = models.TextField(null = True)
+    sizing = models.CharField(null = True, max_length = 5) #for sizes like xxxxl
+    price = models.FloatField(null = True)
+    currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
+    brand = models.CharField(null = True, max_length = 50)
+    image_url = models.URLField(null = True, max_length = 300)
 
-class ScrapedCustomResult(model.Model):
-    product_name = models.TextField()
-    # sizing =  #still researching how to create a custom field for this
-    price = models.FloatField()
-    currency = models.CharField(max_length = 3) #currency like USD, MYR, JPY, THB, etc.
-    brand = models.CharField(max_length = 50)
-    image_url = models.URLField(NULL = True, max_length = 300)
+class ScrapedCustomResult(models.Model):
+    product_name = models.TextField(null = True)
+    #sizing = #still figuring out how to create a custom field for sizing
+    price = models.FloatField(null = True)
+    currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
+    brand = models.CharField(null = True, max_length = 50)
+    image_url = models.URLField(null = True, max_length = 300)
