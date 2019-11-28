015229df903797d2089a145f0633b35c692b3e43
GeraldK <geraldkan99@gmail.com>
2019-11-28 11:37:27 +0800

    registered models so they show in admin panel

--- cwabackend/site_scraper/admin.py
+++ cwabackend/site_scraper/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import StoredResult, ScrapedResult, ScrapedCustomResult
 # Register your models here.
+admin.site.register(StoredResult)
+admin.site.register(ScrapedResult)
+admin.site.register(ScrapedCustomResult)
