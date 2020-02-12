3fbbf4d5e56226b037316e0865645b8de7f9925e
GeraldK <geraldkan99@gmail.com>
2020-02-12 16:00:36 +0800

    Generates a viewable API to work with db and get entries.

--- cwabackend/site_scraper/views.py
+++ cwabackend/site_scraper/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import StoredResult, ScrapedCustomResult, ScrapedResult
+from .serializers import StoredResultSeralizer, ScrapedResultSerializer, ScrapedCustomResultSerialzer
 
-# Create your views here.
+class StoredResultView(viewsets.ModelViewSet):
+    queryset = StoredResult.objects.all()
+    serializer_class = StoredResultSeralizer
+
+class ScrapedResultView(viewsets.ModelViewSet):
+    queryset = ScrapedResult.objects.all()
+    serializer_class = ScrapedResultSerializer
+
+class ScrapedCustomResultView(viewsets.ModelViewSet):
+    queryset = ScrapedCustomResult.objects.all()
+    serializer_class = ScrapedCustomResultSerialzer
