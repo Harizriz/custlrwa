7cfa8dba8c1b98fa7c63396024183a844483246f
GeraldK <geraldkan99@gmail.com>
2020-02-12 15:59:48 +0800

    Serializers to generate JSON files for frontend to use

---
+++ cwabackend/site_scraper/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import StoredResult, ScrapedResult, ScrapedCustomResult
+
+class StoredResultSeralizer(serializers.ModelSerializer):
+    class Meta:
+        model = StoredResult
+        fields = ('id', 'product_name', 'sizing', 'price', 'currency', 'brand', 'image_url')
+
+class ScrapedResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScrapedResult
+        fields = ('id', 'product_name', 'sizing', 'price', 'currency', 'brand', 'image_url')
+
+class ScrapedCustomResultSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = ScrapedCustomResult
+        fields = ('id', 'product_name', 'chest_size', 'waist_size', 'sleeve_length', 'shoulder_size', 'neck_size', 'arm_size', 'price', 'currency', 'brand', 'image_url')
