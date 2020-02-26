7c1f46135036091ff8e5de24ca819a4bd5d4d697
GeraldK <geraldkan99@gmail.com>
2020-02-26 15:18:21 +0800

    scraper script removed, a script to insert to db made instead

--- cwabackend/scraper.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-from bs4 import BeautifulSoup
-
-URL = 'https://www.zalora.com.my/women/clothing/'
-response = get(URL)
-
-# soup = BeautifulSoup(response.content, 'html5lib')
-# print(response.content)
-
