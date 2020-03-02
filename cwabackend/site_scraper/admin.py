7d22ce138cd098b03b8429472fd169fc7a872aaa
GeraldK <geraldkan99@gmail.com>
2020-03-02 13:25:45 +0800

    deleted migrations and rebuilt db due to new models

--- cwabackend/site_scraper/admin.py
+++ cwabackend/site_scraper/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StoredResult, ScrapedResult, ScrapedCustomResult
+from .models import ScrapedResult, ScrapedCustomResult, Sizing
 # Register your models here.
-admin.site.register(StoredResult)
+# admin.site.register(StoredResult)
+admin.site.register(Sizing)
 admin.site.register(ScrapedResult)
 admin.site.register(ScrapedCustomResult)
--- cwabackend/site_scraper/models.py
+++ cwabackend/site_scraper/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class StoredResult(models.Model):
-    product_name = models.TextField(null = True)
-    sizing = models.CharField(null = True, max_length = 5) #for sizes like xxxxl
-    price = models.FloatField(null = True)
-    currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
-    brand = models.CharField(null = True, max_length = 50)
-    image_url = models.URLField(null = True, max_length = 300)
+class Sizing(models.Model):
+    code = models.CharField(null = True, max_length = 5)
+    description = models.CharField(null = True, max_length = 10)
+    
+    def __str__(self):
+        return self.description
+
+# class StoredResult(models.Model):
+#     product_name = models.TextField(null = True)
+#     sizing = models.CharField(null = True, max_length = 5) #for sizes like xxxxl
+#     price = models.FloatField(null = True)
+#     currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
+#     brand = models.CharField(null = True, max_length = 50)
+#     image_url = models.URLField(null = True, max_length = 300)
 
 class ScrapedResult(models.Model):
     product_name = models.TextField(null = True)
-    sizing = models.CharField(null = True, max_length = 5) #for sizes like xxxxl
+    sizing = models.ForeignKey(Sizing, on_delete=models.CASCADE)
     price = models.FloatField(null = True)
     currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
     brand = models.CharField(null = True, max_length = 50)
     image_url = models.URLField(null = True, max_length = 300)
+     
+    def __str__(self):
+        return self.product_name
+    
 
 class ScrapedCustomResult(models.Model):
     product_name = models.TextField(null = True)
