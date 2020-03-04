513af40db3e94b8e2b7dfdd7d201e9bf7ac7f732
Ger4ldK <geraldkan99@gmail.com>
2020-03-05 01:03:21 +0800

    Fixed primary and foreign key issues, no warnings from SQL now

--- cwabackend/site_scraper/models.py
+++ cwabackend/site_scraper/models.py
@@ -1,27 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Sizing(models.Model):
-    code = models.CharField(null = True, max_length = 5)
-    description = models.CharField(null = True, max_length = 10)
-    
-    def __str__(self):
-        return self.description
-
 class ScrapedResult(models.Model):
     product_name = models.TextField(null=True)
-    sizing = models.ForeignKey(Sizing, on_delete=models.CASCADE, null = True)
+    sizing = models.CharField(null = True, max_length=5)
     price = models.FloatField(null = True)
     currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
     brand = models.CharField(null = True, max_length = 50)
     image_url = models.URLField(null = True, max_length = 300)
-     
-    def __str__(self):
-        return self.product_name
+
+class Sizing(models.Model):
+    product_id = models.ForeignKey(ScrapedResult, on_delete=models.CASCADE, null = True, related_name='+')
+    description = models.CharField(null = True, max_length = 10)
 
 class ProductPage(models.Model):
-    product_name = models.OneToOneField(ScrapedResult, on_delete=models.DO_NOTHING, null = True)
-    product_id = models.IntegerField(primary_key=True)
+    product_name = models.TextField(null = True)
+    product_id = models.OneToOneField(ScrapedResult, unique=True, on_delete=models.CASCADE, null = True)
     currency = models.CharField(null=True, max_length=5)
     price = models.FloatField(null=True)
     # images_id = models.ForeignKey(ImageUrl, on_delete=models.DO_NOTHING, null = True)
@@ -33,7 +27,7 @@ class ProductPage(models.Model):
         return self.product_name
 
 class ImageUrl(models.Model):
-    product_id = models.ForeignKey(ProductPage, on_delete=models.DO_NOTHING, null = True)
+    product_id = models.ForeignKey(ScrapedResult, on_delete=models.CASCADE, null = True)
     image_url = models.TextField()
 
     def __str__(self):
@@ -43,7 +37,7 @@ class ImageUrl(models.Model):
         ordering = ['product_id']
 
 class Reviews(models.Model):
-    product_id = models.ForeignKey(ProductPage, on_delete=models.DO_NOTHING, null = True)
+    product_id = models.ForeignKey(ScrapedResult, on_delete=models.CASCADE, null = True)
     reviewer_name = models.TextField()
     summary = models.TextField(max_length=50)
     review = models.TextField(max_length=300)
