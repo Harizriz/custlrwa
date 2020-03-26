2acb637b45f5cb012a1a883d2d02c19d9474865b
Ger4ldK <geraldkan99@gmail.com>
2020-03-27 03:21:15 +0800

    Changed custom results to a custom sizing table as scrapedresults contains same fields. Altered fields for other tables' FK and IntFields

--- cwabackend/site_scraper/models.py
+++ cwabackend/site_scraper/models.py
@@ -10,7 +10,7 @@ class ScrapedResult(models.Model):
     brand = models.CharField(null = True, max_length = 50)
     image_url = models.URLField(null = True, max_length = 300)
     product_description = models.TextField(null=True)
-    original_site = models.TextField(null=True)
+    original_site = models.TextField(null=True, unique=True)
     category = models.TextField(null=True)
     subcategory = models.TextField(null=True)
 
@@ -23,7 +23,7 @@ class ImageUrl(models.Model):
     image_url = models.TextField()
 
 class Reviews(models.Model):
-    product = models.ForeignKey(ScrapedResult, on_delete=models.CASCADE, null = True)
+    product = models.ForeignKey(ScrapedResult, on_delete=models.DO_NOTHING, null = True, to_field='original_site')
     reviewer_name = models.TextField()
     summary = models.TextField(max_length=50)
     review = models.TextField(max_length=300)
@@ -31,23 +31,11 @@ class Reviews(models.Model):
     price = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     value = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
-    def __str__(self):
-        return self.product_id
-    
-    class Meta:
-        ordering = ['product_id']
-
-class ScrapedCustomResult(models.Model):
-    product_name = models.TextField(null = True)
-    # TODO Make custom sizing a foreign key table
+class CustomSizing(models.Model):
+    product = models.ForeignKey(ScrapedResult, on_delete=models.CASCADE, null = True, related_name='custom_sizing')
     chest_size = models.FloatField(null = True)
     waist_size = models.FloatField(null = True)
     sleeve_length = models.FloatField(null = True)
     shoulder_size = models.FloatField(null = True)
     neck_size = models.FloatField(null = True)
     arm_size = models.FloatField(null = True)
-    
-    price = models.FloatField(null = True)
-    currency = models.CharField(null = True, max_length = 3) #currency like USD, MYR, JPY, THB, etc.
-    brand = models.CharField(null = True, max_length = 50)
-    image_url = models.URLField(null = True, max_length = 300)
