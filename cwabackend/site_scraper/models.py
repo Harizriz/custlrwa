78d4ef384901f54142f3c5012417a07c44f72a51
Ger4ldK <geraldkan99@gmail.com>
2020-03-28 16:13:05 +0800

    Changed fields causing issues for db

--- cwabackend/site_scraper/models.py
+++ cwabackend/site_scraper/models.py
@@ -3,14 +3,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class ScrapedResult(models.Model):
-    id = models.BigIntegerField(primary_key = True, null = False)
+    id = models.BigIntegerField(primary_key = True, null = False, unique=True)
     product_name = models.TextField(null=True)
     price = models.DecimalField(null = True, max_digits=9, decimal_places=2)
     currency = models.CharField(null = True, max_length = 10) #currency like USD, MYR, JPY, THB, etc.
     brand = models.CharField(null = True, max_length = 50)
     image_url = models.URLField(null = True, max_length = 300)
     product_description = models.TextField(null=True)
-    original_site = models.TextField(null=True, unique=True)
+    original_site = models.TextField(null=True)
     category = models.TextField(null=True)
     subcategory = models.TextField(null=True)
 
@@ -23,7 +23,7 @@ class ImageUrl(models.Model):
     image_url = models.TextField()
 
 class Reviews(models.Model):
-    product = models.ForeignKey(ScrapedResult, on_delete=models.DO_NOTHING, null = True, to_field='original_site')
+    product = models.ForeignKey(ScrapedResult, on_delete=models.DO_NOTHING, null = True)
     reviewer_name = models.TextField()
     summary = models.TextField(max_length=50)
     review = models.TextField(max_length=300)
