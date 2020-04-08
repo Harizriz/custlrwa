ba2011cbb59dda2faeccc37265337c1c484ada1e
hariz <hariz1807@gmail.com>
2020-04-08 20:55:11 +0800

    started working on custom sizing filter and implemented wishlist and cart notification throughout the website

--- src boighor/js/cart-mini.js
+++ src boighor/js/cart-mini.js
@@ -1,128 +1,18 @@
-// class Cart{
-//     //constructor(products) {
-//     constructor(){
-//        if (!this.storageSupported()){
-//           throw "Browser doesn't support localStorage"
-//        }
-//        // this.products = products;
-//        //this.items = JSON.parse(localStorage.getItem('cart'))
-//     }
-//     find(id, size, source){
-//        for(var i = 0; i < source.length; i++){
-//           if(source[i].id == id && source[i].size == size){
-//              return source[i];
-//           }
-//        }
-//        return false;
-//     }
-//     findSizeIndex(id, source, size){
-//        for(var i = 0; i < source.length; i++){
-//           if(source[i].id == id && source[i].size == size){
-//              return i;
-//           }
-//        }
-//        return false;
-//     }
-//     /*
-//     *Add items to cart
-//     */         
-//     add(id, quantity = 1, size, price, name, page, brand, image, category = 'men'){
-//        //Find product from catalog, build object, add to storage
-//        if (localStorage.getItem('cart') === undefined || localStorage.getItem('cart') === null || JSON.parse(localStorage.getItem('cart')).length === 0){
-//           var item = Object.assign({}, {id: id, quantity: quantity, size: size, price: price, name: name, page: page, brand: brand, image: image, category: category});
-//           localStorage.setItem('cart', JSON.stringify([item]))
-//        }else if(this.find(id, size, JSON.parse(localStorage.getItem('cart'))) != false){
-//           //Item already exists in cart, increase quantity
-//           var cartObj = JSON.parse(localStorage.getItem('cart'));
-//           var oldItem = this.find(id, size, cartObj);
-//           var itemIndex = this.findSizeIndex(id, cartObj, size);
-//           var newItem = Object.assign({}, oldItem, {quantity: (quantity + oldItem.quantity), size: size, price: price, name: name, page: page, brand: brand, image: image, category: category});
-//           cartObj[itemIndex] = newItem;
-//           localStorage.setItem('cart', JSON.stringify(cartObj));
-//        }else{
-//           var item = Object.assign({}, {id: id, quantity: quantity, size: size, price: price, name: name, page: page, brand: brand, image: image, category: category});
-//           var cartObj =  JSON.parse(localStorage.getItem('cart'));
-//           cartObj.push(item)
-//           localStorage.setItem('cart', JSON.stringify(cartObj))
-//        }
-//     }
-//     /*
-//     *Show single item from cart
-//     */
-//     show(id){
-//        return this.find(id, JSON.parse(localStorage.getItem('cart')))
-//     }
-//     /*
-//     *List all items in cart
-//     */
-//     all(){
-//        return  JSON.parse(localStorage.getItem('cart'));
-//     }
-//     /*
-//     *Sum cost of items in cart
-//     *TODO - get price into object
-//     */
-//     sum(){
-//        var sum = 0;
-//        var items = JSON.parse(localStorage.getItem('cart'));
-//        for (var i = 0; i < items.length; i++) {
-//           sum += (items[i].price * items[i].quantity)
-//        }
-//        return sum;
-//     }
-//     /*
-//     *Remove an item from cart
-//     */
-//     remove(id, size){
-       
-//        var items = JSON.parse(localStorage.getItem('cart'));
-//        var index = this.findSizeIndex(id, items, size)
-
-//        if (index >= 0){
-//           items.splice(index, 1)
-//           localStorage.setItem('cart', JSON.stringify(items));
-//        }
-//     }
-//     /*
-//     *Update cart item by its quantity
-//     */
-//     update(id, quantity, size){
-//        var cartObj = JSON.parse(localStorage.getItem('cart'));
-
-//        var oldItem = this.find(id, size, cartObj)
-//        var itemIndex = this.findSizeIndex(id, cartObj, size)
-//        var updatedItem = Object.assign({}, oldItem, {quantity: quantity})
-//        cartObj[itemIndex] = updatedItem;
-
-//        //Save back to storage
-//        localStorage.setItem('cart', JSON.stringify(cartObj))
-//     }
-//     /**
-//     *Remove all items from cart
-//     */
-//     clear(){
-//        localStorage.removeItem('cart')
-//     }
-//     storageSupported(){
-//        return (typeof(Storage) !== "undefined") ? true : false;
-//     }
-// };
-
 var miniCart = new Cart();
-var miniCartData = miniCart.all();
 
 function loadMiniCart(){
+    var miniCartData = miniCart.all();
     var cartCount = '<span>'+ miniCartData.length.toString() +' items</span><span>Cart Subtotal</span>'
-    var cartTotal = 'RM' + miniCart.sum().toString();
+    var cartTotal = 'RM' + miniCart.sum().toFixed(2).toString();
     var miniCartHTML = '';
 
     if (miniCartData.length !=0){
-      for (i=0; i<miniCartData.length; i++){
-         miniCartHTML += '<div class="item01 d-flex"><div class="thumb"><a onclick="openPage(\'' + miniCartData[i].page + '\')"><img src="'+ miniCartData[i].image +'"></a></div>';
-         miniCartHTML += '<div class="content"><h6><a onclick="openPage(\'' + miniCartData[i].page + '\')">'+ miniCartData[i].name +'</a></h6><span class="prize">RM '+ miniCartData[i].price +'</span>';
-         miniCartHTML += '<div class="product_prize d-flex justify-content-between"><span class="qun">Quantity: ' + miniCartData[i].quantity + '</span>';
-         miniCartHTML += '<ul class="d-flex justify-content-end"><li><a onclick="removeFromCart(\''+ miniCartData[i].id + '\', \''+ miniCartData[i].size +'\')"><i class="zmdi zmdi-delete"></i></a></li></ul></div></div></div>';
-      }
+        for (i=0; i<miniCartData.length; i++){
+            miniCartHTML += '<div class="item01 d-flex"><div class="thumb"><a onclick="openPage(\'' + miniCartData[i].page + '\')"><img src="'+ miniCartData[i].image +'"></a></div>';
+            miniCartHTML += '<div class="content"><h6><a onclick="openPage(\'' + miniCartData[i].page + '\')">'+ miniCartData[i].name +'</a></h6><span class="prize">RM '+ miniCartData[i].price +'</span>';
+            miniCartHTML += '<div class="product_prize d-flex justify-content-between"><span class="qun">Quantity: ' + miniCartData[i].quantity + '</span>';
+            miniCartHTML += '<ul class="d-flex justify-content-end"><li><a onclick="removeFromCart(\''+ miniCartData[i].id + '\', \''+ miniCartData[i].size +'\')"><i class="zmdi zmdi-delete"></i></a></li></ul></div></div></div>';
+        }
     }
     else {
         miniCartHTML = '<div class="item01 d-flex">Cart is empty!</div>'
@@ -130,6 +20,20 @@ function loadMiniCart(){
     document.getElementById('cart_total').innerHTML = cartTotal;
     document.getElementById('cart_count').innerHTML = cartCount;
     document.getElementById('mini_cart').innerHTML = miniCartHTML;
+    // update cart notif here
+    getCartNotification();
+}
+
+function getCartNotification() {
+    var empty = '';
+    var miniCartData = miniCart.all();
+    if (miniCartData.length != null) {
+        document.getElementById("cart_noti").innerHTML = miniCartData.length;
+    }
+    else {
+        empty += '<a class="cartbox_active" href="#"></a>';
+        document.getElementById("shopcart").innerHTML = empty;
+    }
 }
 
 loadMiniCart();
--- src boighor/js/sizingChart.js
+++ src boighor/js/sizingChart.js
@@ -3,43 +3,85 @@ var men_tops = [
         "uk": "34",
         "us": "34",
         "eu": "44",
-        "intl": "XS"
+        "intl": "XS",
+        "tops": {
+            "chest": "35.5",
+            "low_waist": "31",
+            "hip": "35.75",
+            "arm": "32.5"
+        }
     },
     {
         "uk": "36",
         "us": "36",
         "eu": "46",
-        "intl": "S"
+        "intl": "S",
+        "tops": {
+            "chest": "37",
+            "low_waist": "32.5",
+            "hip": "37.5",
+            "arm": "33"
+        }
     },
     {
         "uk": "38",
         "us": "38",
         "eu": "48",
-        "intl": "M"
+        "intl": "M",
+        "tops": {
+            "chest": "38.5",
+            "low_waist": "34",
+            "hip": "39",
+            "arm": "33.5"
+        }
     },
     {
         "uk": "40",
         "us": "40",
         "eu": "50",
-        "intl": "lL"
+        "intl": "L",
+        "tops": {
+            "chest": "40.25",
+            "low_waist": "35.5",
+            "hip": "40.5",
+            "arm": "34.25"
+        }
     },
     {
         "uk": "42",
         "us": "42",
         "eu": "52",
-        "intl": "XL"
+        "intl": "XL",
+        "tops": {
+            "chest": "41.75",
+            "low_waist": "37.25",
+            "hip": "42.75",
+            "arm": "34.5"
+        }
     },
     {
         "uk": "44",
         "us": "44",
         "eu": "54",
-        "intl": "XXL"
+        "intl": "XXL",
+        "tops": {
+            "chest": "43.25",
+            "low_waist": "38.75",
+            "hip": "43.75",
+            "arm": "35"
+        }
     },
     {
         "uk": "46",
         "us": "46",
         "eu": "56",
-        "intl": "XXXL"
+        "intl": "XXXL",
+        "tops": {
+            "chest": "44.75",
+            "low_waist": "40.25",
+            "hip": "45.25",
+            "arm": "35.5"
+        }
     }
 ];
 
@@ -48,43 +90,78 @@ var men_trousers = [
         "uk": "28",
         "us": "28",
         "eu": "44",
-        "intl": "XS"
+        "intl": "XS",
+        "trousers": {
+            "low_waist": "31",
+            "hip": "35.75",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "32",
         "us": "32",
         "eu": "46",
-        "intl": "S"
+        "intl": "S",
+        "trousers": {
+            "low_waist": "32.5",
+            "hip": "37.5",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "34",
         "us": "34",
         "eu": "48",
-        "intl": "M"
+        "intl": "M",
+        "trousers": {
+            "low_waist": "34",
+            "hip": "39",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "36",
         "us": "36",
         "eu": "50",
-        "intl": "L"
+        "intl": "L",
+        "trousers": {
+            "low_waist": "35.5",
+            "hip": "40.5",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "38",
         "us": "38",
         "eu": "52",
-        "intl": "XL"
+        "intl": "XL",
+        "trousers": {
+            "low_waist": "37.25",
+            "hip": "42.25",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "40",
         "us": "40",
         "eu": "54",
-        "intl": "XXL"
+        "intl": "XXL",
+        "trousers": {
+            "low_waist": "38.75",
+            "hip": "43.75",
+            "inner_leg": "33.5"
+        }
     },
     {
         "uk": "42",
         "us": "42",
         "eu": "56",
-        "intl": "XXXL"
+        "intl": "XXXL",
+        "trousers": {
+            "low_waist": "40.25",
+            "hip": "45.25",
+            "inner_leg": "33.5"
+        }
     }
 ];
 
@@ -130,6 +207,173 @@ var women = [
         "us": "10",
         "eu": "42",
         "intl": "L"
+    }
+];
+
+var women_tops = [
+    {
+        "uk": "2",
+        "us": "00",
+        "eu": "30",
+        "intl": "XS",
+        "tops":{
+            "bust": "30",
+            "waist": "23",
+            "hip": "33",
+            "arm": "30"
+        }
+    },
+    {
+        "uk": "4",
+        "us": "0",
+        "eu": "32",
+        "intl": "XS",
+        "tops":{
+            "bust": "31",
+            "waist": "24",
+            "hip": "34",
+            "arm": "30"
+        }
+    },
+    {
+        "uk": "6",
+        "us": "2",
+        "eu": "34",
+        "intl": "S",
+        "tops":{
+            "bust": "32",
+            "waist": "25",
+            "hip": "35",
+            "arm": "30.75"
+        }
+    },
+    {
+        "uk": "8",
+        "us": "4",
+        "eu": "36",
+        "intl": "S",
+        "tops":{
+            "bust": "33",
+            "waist": "26",
+            "hip": "36",
+            "arm": "30.75"
+        }
+    },
+    {
+        "uk": "10",
+        "us": "6",
+        "eu": "38",
+        "intl": "M",
+        "tops":{
+            "bust": "35",
+            "waist": "28",
+            "hip": "38",
+            "arm": "31.25"
+        }
+    },
+    {
+        "uk": "12",
+        "us": "8",
+        "eu": "40",
+        "intl": "M",
+        "tops":{
+            "bust": "37",
+            "waist": "30",
+            "hip": "40",
+            "arm": "31.25"
+        }
+    },
+    {
+        "uk": "14",
+        "us": "10",
+        "eu": "42",
+        "intl": "L",
+        "tops":{
+            "bust": "39",
+            "waist": "32",
+            "hip": "42",
+            "arm": "31.5"
+        }
+    }
+];
+
+var women_trousers = [
+    {
+        "uk": "2",
+        "us": "00",
+        "eu": "30",
+        "intl": "XS",
+        "trousers":{
+            "low_waist": "30.75",
+            "hip": "35.5",
+            "inner_leg": "32.25"
+        }
+    },
+    {
+        "uk": "4",
+        "us": "0",
+        "eu": "32",
+        "intl": "XS",
+        "trousers":{
+            "low_waist": "30.75",
+            "hip": "35.5",
+            "inner_leg": "32.25"
+        }
+    },
+    {
+        "uk": "6",
+        "us": "2",
+        "eu": "34",
+        "intl": "S",
+        "trousers":{
+            "low_waist": "32.25",
+            "hip": "37",
+            "inner_leg": "32.5"
+        }
+    },
+    {
+        "uk": "8",
+        "us": "4",
+        "eu": "36",
+        "intl": "S",
+        "trousers":{
+            "low_waist": "32.25",
+            "hip": "37",
+            "inner_leg": "32.5"
+        }
+    },
+    {
+        "uk": "10",
+        "us": "6",
+        "eu": "38",
+        "intl": "M",
+        "trousers":{
+            "low_waist": "33.75",
+            "hip": "38.5",
+            "inner_leg": "33"
+        }
+    },
+    {
+        "uk": "12",
+        "us": "8",
+        "eu": "40",
+        "intl": "M",
+        "trousers":{
+            "low_waist": "33.75",
+            "hip": "38.5",
+            "inner_leg": "33"
+        }
+    },
+    {
+        "uk": "14",
+        "us": "10",
+        "eu": "42",
+        "intl": "L",
+        "trousers":{
+            "low_waist": "35.5",
+            "hip": "40",
+            "inner_leg": "33.5"
+        }
     }
 ];
 
@@ -195,7 +439,7 @@ function sizingDetection(sizeType, gender, category){
     return sizingType;
 }
 
-function sizingList(sizing_original, sizeType, gender){ //sizing given from the backend object
+function sizingList(sizing_original, sizeType, gender){
     var sizing_list = []
     var category = 'tops'; //once we get subcategory in the db this can be removed and added as arg for the function
     var sizingType = sizingDetection(sizeType, gender, category)
@@ -303,4 +547,28 @@ function sizingList(sizing_original, sizeType, gender){ //sizing given from the 
             }
     }
     return sizing_list
+}
+
+//todo functions for comparison of user sizes and sizes in var
+function sizing_match(user_sizing, size, type, gender){ // user measurements, available sizes(sizing_chart), type(for now let users select since backend doesnt have it), gender
+    if (type == 'tops'){
+        top_match(user_sizing, size, gender);
+    }
+    else{
+        trouser_match(user_sizing, size, gender);
+    }
+};
+
+function top_match(user, size, gender){
+    //get difference between specific lengths, then calculate match for sizing based on weightage of the size
+    if (gender == 'men'){
+        tops_men(user, size);
+    }
+    else{
+        tops_women(user, size);
+    }
+}
+
+function tops_men(user, size){
+    var sizingDifference = [];
 }
